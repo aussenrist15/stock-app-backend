1e51ba2b426f91d173c872ddac76edb8049b09bd
Abdul Basit <chabdulbasit1122@gmail.com>
2020-10-12 01:14:24 +0500

    Setup routes functioning with dummy data

--- Routes/transactions.js
+++ Routes/transactions.js
@@ -1,25 +1,51 @@
 const express = require("express");
 const router = express.Router();
 
+const data = [
+  {
+    id: 1,
+    name: "Basit",
+  },
+  {
+    id: 2,
+    name: "Ayesha",
+  },
+  {
+    id: 3,
+    name: "Ashu",
+  },
+  {
+    id: 4,
+    name: "Chay",
+  },
+];
+
 router
   .route("/")
   .get((req, res) => {
-    res.send("GET REQUEST RECEIVED");
+    res.send(data);
     // TODO get all the transactions from database
   })
   .post((req, res) => {
-    res.send("POST REQUEST RECEIVED");
+    console.log(typeof req.body);
+    data.push(req.body);
+    res.send("Ok hogya jee");
     // TODO add a transaction into the database
   });
 
 router
   .route("/:id")
   .delete((req, res) => {
-    res.send("DEL REQUEST RECEIVED");
+    const { id } = req.params;
+    const obje = data.filter((item) => item.id != id);
+
+    res.send(obje);
     // TODO delete a transaction from the database
   })
   .get((req, res) => {
-    res.send("GET REQUEST WITH A PARAMETER");
+    const { id } = req.params;
+    const obje = data.filter((item) => item.id == id);
+    res.send(obje);
     // TODO get all transactions with the user ID of @param id
   });
 
