b3b13edd17c4f031fc6f58ac072386db23cf5caf
Abdul Basit <chabdulbasit1122@gmail.com>
2020-10-13 01:19:43 +0500

    connected database

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const transactionsAPI = require("./Routes/transactions");
 const customersAPI = require("./Routes/customers");
 const stockAPI = require("./Routes/stock");
+const connectDB = require("./config/db");
 
 const app = express();
 app.use(express.json());
+dotenv.config({ path: "./config/config.env" });
+connectDB();
 
 app.use("/api/v1/transactions", transactionsAPI);
 app.use("/api/v1/customers", customersAPI);
